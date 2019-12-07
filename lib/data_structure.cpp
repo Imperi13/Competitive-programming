effa573e08ab63f8233c87cd3de4308cd104c673
Imperi <imperi.night@gmail.com>
2019-12-07 20:56:59 +0900

    add SWAG

--- lib/data_structure.cpp
+++ lib/data_structure.cpp
@@ -252,7 +252,7 @@ class DynamicSegmentTree{
 };
 
 //LiChaoSegmentTree
-//CHTができる
+//minを返すCHTができる
 template<typename T,T id>
 class LiChaoSegmentTree{
 
@@ -375,6 +375,52 @@ class LiChaoSegmentTree{
     if(now->line.value(num[i]) < ans)ans=now->line.value(num[i]);
 
     return ans;
+  }
+};
+
+//SWAG スライド区間上の演算ができる
+template<typename T>
+class SWAG{
+  private:
+
+  std::function<T(T,T)> fn;
+
+  std::stack<T> front,back;
+  std::vector<T> front_fold,back_fold;
+
+  T id;
+
+  public:
+
+  SWAG(T id_,std::function<T(T,T)> fn_):id(id_),fn(fn_),front_fold(1,id_),back_fold(1,id_){}
+
+  void push(T val){
+    back.push(val);
+    back_fold.push_back(fn(back_fold.back(),val));
+  }
+
+  void pop(){
+    if(front.empty()){
+      assert(front_fold.size()==1);
+      while(!back.empty()){
+        T temp=back.top();
+        back.pop();
+        back_fold.pop_back();
+
+        front.push(temp);
+        front_fold.push_back(fn(temp,front_fold.back()));
+      }
+    }
+
+    assert(!front.empty());
+
+    front.pop();
+    front_fold.pop_back();
+    
+  }
+
+  T fold_all(){
+    return fn(front_fold.back(),back_fold.back());
   }
 };
 

bfe1f52a2a8aae736460ffb5a6ec64f51ed820c4
Imperi <imperi.night@gmail.com>
2020-01-03 21:02:35 +0900

    fix LiChaoSegmentTree

--- lib/SegmentTree/LiChaoSegmentTree.cpp
+++ lib/SegmentTree/LiChaoSegmentTree.cpp
@@ -22,145 +22,122 @@
 #include <limits>
 #include "limits.h"
 
-//LiChaoSegmentTree
-//minを返すCHTができる 線分追加に対応した
-//PS:線分追加バグってる
-template<typename T,T id>
+//LiChaoSegmentTree 直線追加 O(logN) 線分追加 O(log^2 N)? 最小値クエリ O(log N)
+//templateのMAXはLineの初期値として使う (直線の値域は[-INF,MAX)となる必要がある)
+template<typename T,T MAX>
 class LiChaoSegmentTree{
+  using P=std::pair<T,T>;
 
   struct Line{
-    std::pair<T,T> coeffi;
-
-    Line(std::pair<T,T> a):coeffi(a){}
-
-    T value(T x){
-      return coeffi.first*x+coeffi.second;
-    }
+    P a;
+    T value(T x){return a.first*x+a.second;}
+    Line():a(0,MAX){}
+    Line(P a_):a(a_){}
   };
 
+  struct Node;
+  using node_ptr=std::shared_ptr<Node>;
+
   struct Node{
-
-    std::shared_ptr<Node> left,right;
+    node_ptr left,right;
     Line line;
-
-    Node():line(std::pair<T,T>(0,id)),left(),right(){}
-
-    Node(Line line_):line(line_),left(),right(){}
+    Node():left(),right(),line(){}
   };
 
   private:
 
   long long n,n0;
-  std::vector<T> num;
+  node_ptr root;
+  std::vector<T> x;
 
-  std::shared_ptr<Node> root;
-
-  void eval(Line f,long long a,long long b,std::shared_ptr<Node> now,long long l,long long r){
-
+  void eval(Line f,long long a,long long b,node_ptr now,long long l,long long r){
     if(a==b)return;
-
-    if(r-l>1){
-      if(f.value(num[l])>=now->line.value(num[l])&&f.value(num[std::min(n,r)-1])>=now->line.value(num[std::min(n,r)-1])){
+    if(r<=a||b<=l)return;
+    if(f.value(x[l]) >= now->line.value(x[l]) && f.value(x[r-1]) >= now->line.value(x[r-1]))return;
+    if(r-l==1){
+      if(f.value(x[l]) < now->line.value(x[l])){
+        std::swap(f,now->line);
         return;
       }
+    }
 
-      long long mid=l+(r-l)/2;
+    long long mid=l+(r-l)/2;
 
-      if(a<=l&&r<=b){
-
-        if(n<=mid){
-          if(now->line.value(num[n-1]) > f.value(num[n-1])){
-            std::swap(now->line,f);
-          }
-
-          if(!now->left)now->left=std::make_shared<Node>();
-          eval(f,a,b,now->left,l,mid);
-        }else{
-          if(now->line.value(num[mid-1]) > f.value(num[mid-1])){
-            std::swap(now->line,f);
-          }
-
-          if(now->line.value(num[l]) > f.value(num[l])){
-            if(!now->left)now->left=std::make_shared<Node>();
-            eval(f,a,b,now->left,l,mid);
-          }else{
-            if(!now->right)now->right=std::make_shared<Node>();
-            eval(f,a,b,now->right,mid,r);
-          }
-        }
+    if(a<=l&&r<=b){
+      if(f.value(x[l]) < now->line.value(x[l]) && f.value(x[r-1]) < now->line.value(x[r-1])){
+        std::swap(f,now->line);
+        return;
+      }
+      if(f.value(x[mid-1]) < now->line.value(x[mid-1]))std::swap(f,now->line);
+      if(f.value(x[l]) < now->line.value(x[l])){
+        if(!now->left)now->left=std::make_shared<Node>();
+        eval(f,a,b,now->left,l,mid);
       }else{
-        if(b>l&&mid>a){
-          if(!now->left)now->left=std::make_shared<Node>();
-          eval(f,a,b,now->left,l,mid);
-        }
-        if(b>mid&&r>a){
-          if(!now->right)now->right=std::make_shared<Node>();
-          eval(f,a,b,now->right,mid,r);
-        }
+        if(!now->right)now->right=std::make_shared<Node>();
+        eval(f,a,b,now->right,mid,r);
       }
     }else{
-      if(now->line.value(l) > f.value(l)){
-        now->line=f;
-        return;
+      if(mid>a&&b>l){
+        if(!now->left)now->left=std::make_shared<Node>();
+        eval(f,a,b,now->left,l,mid);
+      }
+      if(r>a&&b>mid){
+        if(!now->right)now->right=std::make_shared<Node>();
+        eval(f,a,b,now->right,mid,r);
       }
     }
   }
 
   public:
 
-  LiChaoSegmentTree(long long n_):n(n_),root(new Node()),num(std::vector<T>(n_,0)){
-    for(ll i=1;i<n_;i++)num[i]=num[i-1]+1;
-
-    n0=1;
-    while(n0<n_)n0<<=1;
-  }
-
-  LiChaoSegmentTree(const std::vector<T>& num_):n(num_.size()),root(new Node()),num(num_){
+  //x座標を[0,n_)で初期化
+  LiChaoSegmentTree(long long n_):n(n_),root(new Node()){
     n0=1;
     while(n0<n)n0<<=1;
+    x=std::vector<T>(n0,n-1);
+    for(long long i=0;i<n;i++)x[i]=i;
   }
 
-
-
-  void addline(std::pair<T,T> line_){
-    Line f=Line(line_);
-    eval(f,0,n0,root,0,n0);
+  //ソート済みx座標を渡す
+  LiChaoSegmentTree(const std::vector<T>& x_):n(x_.size()),root(new Node()){
+    n0=1;
+    while(n0<n)n0<<=1;
+    x=std::vector<T>(n0,x_[n-1]);
+    for(long long i=0;i<n;i++)x[i]=x_[i];
   }
 
-  void addsegment(std::pair<T,T> line_,T a,T b){
-    Line f=Line(line_);
-    long long first=(std::lower_bound(all(num),a)-num.begin());
-    long long second=(std::lower_bound(all(num),b)-num.begin());
-    eval(f,first,second,root,0,n0);
+  void add_line(P line_){
+    eval(Line(line_),0,n0,root,0,n0);
+  }
+
+  void add_segment(P line_,T lval,T rval){
+    long long l=(std::lower_bound(x.begin(),x.end(),lval)-x.begin());
+    long long r=(std::lower_bound(x.begin(),x.end(),rval)-x.begin());
+    eval(Line(line_),l,r,root,0,n0);
   }
 
   T query(T val){
-    long long i=std::lower_bound(all(num),val)-num.begin();
-    std::shared_ptr<Node> now(root);
+    long long itr=(std::lower_bound(x.begin(),x.end(),val)-x.begin());
+    assert(itr<n&&x[itr]==val);
 
-    T ans=now->line.value(num[i]);
-
+    T ans=MAX;
+    node_ptr now(root);
     long long l=0,r=n0;
+
     while(r-l>1){
-      if(ans > now->line.value(num[i]))ans=now->line.value(num[i]);
+      if(ans>now->line.value(val))ans=now->line.value(val);
       long long mid=l+(r-l)/2;
-
-      if(i<mid){
-        r=mid;
+      if(itr<mid){
         if(!now->left)return ans;
-
         now=now->left;
+        r=mid;
       }else{
-        l=mid;
         if(!now->right)return ans;
-
         now=now->right;
+        l=mid;
       }
-
     }
-
-    if(now->line.value(num[i]) < ans)ans=now->line.value(num[i]);
-
+    if(ans>now->line.value(val))ans=now->line.value(val);
     return ans;
   }
 };

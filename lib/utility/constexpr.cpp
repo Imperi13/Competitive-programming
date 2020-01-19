6e4c5d4dbc52aa3e30d8e9d069aa16a7f08ddaf2
Imperi <imperi.night@gmail.com>
2020-01-20 03:53:39 +0900

    add constexpr

--- lib/utility/constexpr.cpp
+++ lib/utility/constexpr.cpp
@@ -21,3 +21,18 @@
 #include <memory>
 #include <utility>
 #include <limits>
+
+//NまでのMOD逆元をコンパイル時計算する
+template<long long N,long long MOD>
+class ModInv_calc{
+  public:
+  long long inv[N+1];
+  constexpr ModInv_calc():inv(){
+    inv[1]=1;
+    for(int i=2;i<=N;i++)inv[i]=inv[MOD%i]*(MOD-MOD/i)%MOD;
+  }
+
+  constexpr long long operator[](size_t i)const noexcept{return inv[i];}
+};
+
+constexpr ModInv_calc<1000,1000000007> inv{ModInv_calc<1000,1000000007>()};
